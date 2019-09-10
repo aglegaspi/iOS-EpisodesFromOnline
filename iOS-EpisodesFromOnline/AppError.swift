570bf3e842a1d89b3c04f71c37363ddfbee0479e
aglegaspi <lex83nyc@gmail.com>
2019-09-10 15:27:39 -0400

    added NetworkManager model and AppError handler

---
+++ iOS-EpisodesFromOnline/AppError.swift
@@ -0,0 +1,9 @@
+//
+//  AppError.swift
+//  iOS-EpisodesFromOnline
+//
+//  Created by Alexander George Legaspi on 9/10/19.
+//  Copyright © 2019 Alexander George Legaspi. All rights reserved.
+//
+
+import Foundation
