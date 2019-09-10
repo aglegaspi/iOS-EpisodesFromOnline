e664d1cb035554b02cf83dea8447506db24acb4a
aglegaspi <lex83nyc@gmail.com>
2019-09-10 15:37:29 -0400

    created custom Show Cell in Main VC for Table View

--- iOS-EpisodesFromOnline/AppError.swift
+++ iOS-EpisodesFromOnline/AppError.swift
@@ -1,9 +1,13 @@
-//
-//  AppError.swift
-//  iOS-EpisodesFromOnline
-//
-//  Created by Alexander George Legaspi on 9/10/19.
-//  Copyright © 2019 Alexander George Legaspi. All rights reserved.
-//
-
 import Foundation
+
+enum AppError: Error {
+    case badJSONError
+    case networkError
+    case noDataError
+    case badHTTPResponse
+    case badUrl
+    case notFound //404 status code
+    case unauthorized //403 and 401 status code
+    case badImageData
+    case other(errorDescription: String)
+}
---
+++ iOS-EpisodesFromOnline/ShowTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ShowTableViewCell.swift
+//  iOS-EpisodesFromOnline
+//
+//  Created by Alexander George Legaspi on 9/10/19.
+//  Copyright © 2019 Alexander George Legaspi. All rights reserved.
+//
+
+import UIKit
+
+class ShowTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var showImage: UIImageView!
+    @IBOutlet weak var showName: UILabel!
+    @IBOutlet weak var showRating: UILabel!
+    
+}
